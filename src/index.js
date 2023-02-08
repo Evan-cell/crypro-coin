972e5d167da77250e2930d93c2093340bc6eda55
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-08 22:35:59 +0300

    crypto api

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import Store from './app/Store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <Provider store={Store}>
     <App />
+      </Provider>
     </Router>
    
   </React.StrictMode>
--- src/services/Cryptoapi.js
+++ src/services/Cryptoapi.js
@@ -1,3 +1,22 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit'
+
+const cryptoApiHeaders = {
+        'X-RapidAPI-Key': '71375eced9msh7c6b37227e6be82p17f352jsn2dd1978a502b',
+        'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+}
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+
+const createRequest = (url) => ({url,Headers: cryptoApiHeaders})
+
+export const Cryptoapi =createApi({
+    reducerPath: 'Cryptoapi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) =>({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
 // const options = {
 //     method: 'GET',
 //     url: 'https://coinranking1.p.rapidapi.com/coins',
