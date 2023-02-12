7f7da1a2e9b59513391cc3bf1ef0add87ef79628
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-13 00:18:45 +0300

    api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Typography,Select,Row,Col,Avatar,Card } from 'antd'
 import moment from 'moment/moment'
-import { useGetCryptosNewsQuery } from '../services/CryptoNewsApi'
+
 
 const {Text, Title} = Typography
 const {Option} = Select
 function News({simplified}) {
-  const {data: cryptoNews} = useGetCryptosNewsQuery({AllNews:'CryptoCurrency', count: simplified ? 10 : 100})
-  console.log(cryptoNews)
+ 
   return (
     <div>News</div> 
   )
