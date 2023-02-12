812b1bd67c0e52a0c665fc005bbca77986bf8018
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-02-13 00:22:00 +0300

    using axios

--- src/services/Api.js
+++ src/services/Api.js
@@ -2,6 +2,14 @@ import axios from 'axios'
 export default {
     getData: ()=>
     axios({
-        'method':'GET'
+        method: 'GET',
+        url: 'https://crypto-news16.p.rapidapi.com/news/all',
+        headers: {
+          'X-RapidAPI-Key': '71375eced9msh7c6b37227e6be82p17f352jsn2dd1978a502b',
+          'X-RapidAPI-Host': 'crypto-news16.p.rapidapi.com'
+        },
+        'params': {
+            'search': 'parameter'
+        }
     })
 }
